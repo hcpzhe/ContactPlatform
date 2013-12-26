0e71a76cf429bcfe545b975ff437e532a501d57c
hcpzhe <zheyun999@163.com>
2013-12-26 16:05:25 +0800

    文件上传路径的检测

--- index.php
+++ index.php
@@ -17,5 +17,13 @@ if (!is_dir($path)) mkdir($path);
 $path = APP_PATH.'Session/Home/';
 if (!is_dir($path)) mkdir($path);
 
+//编辑器内容 所用图片上传路径
+$path = APP_PATH.'Public/textUpload/';
+if (!is_dir($path)) mkdir($path);
+
+//项目_upload文件上传路径
+$path = APP_PATH.'Public/Uploads/';
+if (!is_dir($path)) mkdir($path);
+
 //加载框架入口文件
 require './ThinkPHP/ThinkPHP.php';
