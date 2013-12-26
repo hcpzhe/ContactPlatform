7084e253a1c6a99a5d6b3722a67526d6fae298d7
hcpzhe <zheyun999@163.com>
2013-12-26 11:28:59 +0800

    建议管理发现问题，已修复

--- Lib/Action/Admin/SugreplyAction.class.php
+++ Lib/Action/Admin/SugreplyAction.class.php
@@ -11,14 +11,18 @@ class SugreplyAction extends CommonAction {
     		//自动验证不通过
     		$this->error($sugreply_M->getError());
     	}else{
-    		if (false !== $sugreply_M->add()){
-				$this->success('回复成功',cookie('_currentUrl_'));
+    		$list = $sugreply_M->add();
+    		if (false !== $list){
+   				$sug_id = $sugreply_M->where("id=".$list)->getField("sug_id");
 				$suggest_M = M('Suggest');
-				$info = $suggest_M->where("id={$sugreply_M->sug_id}")->find();
+				$info = $suggest_M->where("id=".$sug_id)->find();
+				
 				if ($info['status']==2){//更新建议审核状态
+					$info['user_id']=$_SESSION[C('USER_AUTH_KEY')];
 					$info['status']=1;
 					$suggest_M->save($info);
-				}    		
+				}
+				$this->success('回复成功',cookie('_currentUrl_'));    		
     		}else{
     			$this->error('回复失败');
     		}
